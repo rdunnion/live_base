2648168ffdd499d91b634a9dcc062a2a1dc18ecc
rdunnion <52118274+rdunnion@users.noreply.github.com>
2022-03-05 20:23:46 -0600

    Fixed Homepage and banner in header.html

--- home.php
+++ home.php
@@ -1,43 +1,27 @@
     <!--- includes contents of file header.html --->
     <?php include "header.html" ?>
 
-    <div class="d-flex justify-content-around">
-        <div class="d-inline-flex p-2 bd-highlight">
-            <p class="text-center">Center aligned text on all viewport sizes.</p>
+    <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <img src="LiveBaseLogo-sm.png" class="img-fluid">
         </div>
-        <div class="d-inline-flex p-2 bd-highlight">
-            <p class="text-center">Center aligned text on all viewport sizes.</p>
-        </div>
-        <div class="d-inline-flex p-2 bd-highlight">
-            <p class="text-center">Center aligned text on all viewport sizes.</p>
-        </div>
+        <div class="col-lg-8 col-md-8 col-sm-12 desc enter_font">
+            <br><br><br>
+            <h3>About Live Base</h3>
+            <p>Live Base is a project to allow live music collectors to store information about thier<br>
+                collections in a database. The database entries can be searched by every registered user.<br>
+                The users passwords are protected by hashing the users passwords prior to storing<br>
+                them in the database. This protects the passwords from being viewed in plain text.<br>
+            </p>
+            <h3>About The Developer</h3>
+            <p>
+                Ryan is in a Bachelor of Science degree program with a focus in software development.<br>
+                This project is his capstone project near the end of his degree. He lives in the midwest<br>
+                and is currently working as a Datacenter Technician for a managed services company based<br>
+                out of Silicon Valley.
+            </p>
+            </div>
     </div>
-
-    <div class="d-flex justify-content-around">
-        <div class="d-inline-flex p-2 bd-highlight">
-            <p class="text-center">Center aligned text on all viewport sizes.</p>
-        </div>
-        <div class="d-inline-flex p-2 bd-highlight">
-            <p class="text-center">Center aligned text on all viewport sizes.</p>
-        </div>
-        <div class="d-inline-flex p-2 bd-highlight">
-            <p class="text-center">Center aligned text on all viewport sizes.</p>
-        </div>
-    </div>
-
-    <div class="d-flex justify-content-around">
-        <div class="d-inline-flex p-2 bd-highlight">
-            <p class="text-center">Center aligned text on all viewport sizes.</p>
-        </div>
-        <div class="d-inline-flex p-2 bd-highlight">
-            <p class="text-center">Center aligned text on all viewport sizes.</p>
-        </div>
-        <div class="d-inline-flex p-2 bd-highlight">
-            <p class="text-center">Center aligned text on all viewport sizes.</p>
-        </div>
-    </div>
-
-    </style>
 
     <!--- includes contents of file footer.html --->
     <?php include "footer.html" ?>
